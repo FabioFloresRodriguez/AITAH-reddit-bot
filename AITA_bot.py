d9d998336c736770e50562d9ae60dc2bc485c593
Fabio Flores Rodriguez <fabiojfloresrodriguez@gmail.com>
2025-06-16 00:36:19 -0400

    updated .gitignore and AITA_bot.py
    changed how the word document will be made, made a new directory for the word documents, and added the directory in .gitignore

--- AITA_bot.py
+++ AITA_bot.py
@@ -7,16 +7,11 @@ from original_title import Original_Title
 
 reddit = praw.Reddit("bot1")
 
-docx_path = Path("AITAH stories.docx")
+docx_path = Path(f"Weekly AITAH stories/{str(date.today())} AITAH stories.docx")
 
-if docx_path.is_file():
-    doc=docx.Document(docx_path)
-    doc.add_page_break()
-    doc.add_heading(str(date.today()),0)
-else:
-    doc = docx.Document()
-    doc.save(docx_path)
-    doc.add_heading(str(date.today()),0) 
+doc = docx.Document()
+doc.save(docx_path)
+doc.add_heading(str(date.today()),0) 
 
 subreddit = reddit.subreddit("AITAH")
 for submission in subreddit.hot(limit=10):
